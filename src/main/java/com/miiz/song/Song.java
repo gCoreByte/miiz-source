901c4677458a860f3199a209939c46966f5b15f0
Anna Maria <anna.maria.tammin@ut.ee>
2022-04-04 22:41:08 +0300

    Songs

--- src/main/java/com/miiz/song/Song.java
+++ src/main/java/com/miiz/song/Song.java
@@ -1,29 +1,20 @@
 package com.miiz.song;
 
-import java.util.List;
-
 public class Song {
 
-    private Genre genre;
+
     private String title;
     private String author;
-    private long id;
     private String url;
+    private Genre genre;
+    private long id;
 
-    public Song(Genre genre, String title, String author, long id, String url) {
-        this.genre = genre;
+    public Song(String title, String author, String url, Genre genre, long id) {
         this.title = title;
         this.author = author;
-        this.id = id;
         this.url = url;
-    }
-
-    public Genre getGenre() {
-        return genre;
-    }
-
-    public void setGenre(Genre genre) {
         this.genre = genre;
+        this.id = id;
     }
 
     public String getTitle() {
@@ -42,14 +33,6 @@ public class Song {
         this.author = author;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
     public String getUrl() {
         return url;
     }
@@ -58,20 +41,30 @@ public class Song {
         this.url = url;
     }
 
-    public void printSongs(List<Song> songsList){
-        for (int i = 1; i < songsList.size(); i++) {
-            System.out.println(i + ". " + songsList.get(i-1).getTitle() + " " + songsList.get(i-1).getAuthor() + " poolt");
-        }
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     @Override
     public String toString() {
         return "Song{" +
-                "genre=" + genre +
-                ", title='" + title + '\'' +
+                "title='" + title + '\'' +
                 ", author='" + author + '\'' +
-                ", id=" + id +
                 ", url='" + url + '\'' +
+                ", genre=" + genre +
+                ", id=" + id +
                 '}';
     }
 }
--- src/main/java/com/miiz/song/SongHandler.java
+++ src/main/java/com/miiz/song/SongHandler.java
@@ -1,40 +1,70 @@
 package com.miiz.song;
 
-import com.miiz.database.Database;
 
 import java.awt.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class SongHandler {
 
-    private final Database database;
-
-    public SongHandler(Database database) {
-        this.database = database;
+    public void printSongs(List<Song> songsList){
+        for (int i = 1; i < songsList.size(); i++) {
+            System.out.println(i + ". " + songsList.get(i-1).getTitle() + " " + songsList.get(i-1).getAuthor() + " poolt");
+        }
     }
 
-    public void main() {
-
-        System.out.println("Lugude valik:");
-
+    public void play(Song song){
+        // https://stackoverflow.com/questions/5226212/how-to-open-the-default-webbrowser-using-java
+        if (!Desktop.isDesktopSupported() || !Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+            System.out.println("Teie arvuti ei toeta seda funktsionaalsust kahjuks.");
+        }
+        try {
+            Desktop.getDesktop().browse(new URI(song.getUrl()));
+        } catch (URISyntaxException e) {
+            System.out.println("Lugu " + song.getTitle() + " pole võimalik esitada");
+        } catch (Exception e) {
+            System.out.println("Something went wrong.");
+        }
     }
 
-    public void play(int pick){
+    public void main(){
 
-    }
+        Scanner inputReader = new Scanner(System.in);
+        List<Song> songs = new ArrayList<>();
+        System.out.println("Vali tegevus:");
+        System.out.println();
+        System.out.println("1 - mängi juhuslik lugu");
+        System.out.println("2 - vali lugu");
+        System.out.println("3 - vali žanr");
+        System.out.println("Sisesta tegevusele vastav number: ");
+        System.out.println();
 
-    public void printList(){
+        String input = inputReader.nextLine().strip();
 
-    }
+        while (true){
+            switch (input) {
+                case "1" -> {
+                    System.out.println("Esitan juhusliku loo");
+                    int rand = (int) (Math.random() * songs.size());
+                    System.out.println("Valisin " + songs.get(rand-1).getTitle());
+                    play(songs.get(rand));
+                }
 
-    public int pickRandom(){
+                case "2" -> {
+                    System.out.println("Vali lugu, mille soovid esitada:");
+                    printSongs(songs);
+                    int pick = Integer.parseInt(inputReader.nextLine().strip());
+                    play(songs.get(pick -1));
+                }
 
-        return 0;
-    }
-
-    public String pickGenre(){
-
-        return null;
+                case "3" -> {
+                    System.out.println(java.util.Arrays.asList(Genre.values()));
+                }
+            }
+        }
     }
 
 }
