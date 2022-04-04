d612ad1a4519449591debc09354a6978695f0901
gCoreByte <21132799+gCoreByte@users.noreply.github.com>
2022-04-04 15:38:12 +0300

    logging in backend, todolist requires ownerid

--- src/main/java/com/miiz/auth/UserAuth.java
+++ src/main/java/com/miiz/auth/UserAuth.java
@@ -1,20 +1,27 @@
 package com.miiz.auth;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
+import com.miiz.database.Database;
 
 /**
- * General user authentication class
+ * General user authentication class (server-side)
  */
 public class UserAuth {
 
     // user token and secret - "remember me"
     private String userToken;
     private String userSecret;
+    private User user; // temporary logged in user while we do not have web auth
     private final Database database;
 
-    public UserAuth(String userToken, String userSecret) {
+    public UserAuth(Database database) {
+        this.database = database;
+    }
+
+    public UserAuth(String userToken, String userSecret, Database database) {
         this.userToken = userToken;
         this.userSecret = userSecret;
+        this.database = database;
     }
     //TODO
 
@@ -25,8 +32,23 @@ public class UserAuth {
      * @param password - user password to log in with
      * @return boolean - true if success, false if error
      */
+    // TODO: move user authentication to server
     public boolean userLogin(String username, String password) {
-        String hashedPw;
+        User user = database.getUserByUsername(username);
+        // id -1 means no user found
+        if (user.getId() == -1) {
+            return false;
+        }
+        String hashedPw = user.getHashedPw();
+        BCrypt.Result result = BCrypt.verifyer().verify(password.toCharArray(), hashedPw);
+        if (result.verified) {
+            // TODO: user secret+token doesn't matter right now - this is important for serverside auth
+            setUserSecret("temp");
+            setUserToken("temp");
+            this.user = user;
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -34,10 +56,19 @@ public class UserAuth {
      *
      * @param username - user email to register with
      * @param password - user password to log in with
-     * @return boolean - true if succes, false if error
+     * @return boolean - true if success, false if error
      */
     public boolean userRegister(String username, String password) {
-        return false;
+        User user = database.getUserByUsername(username);
+        // id -1 means no user found
+        if (user.getId() != -1) {
+            // cant register duplicate username
+            return false;
+        }
+        user = new User(username, BCrypt.withDefaults().hashToString(12, password.toCharArray()));
+        database.addUser(user);
+        return true;
+
     }
 
     public String getUserToken() {
