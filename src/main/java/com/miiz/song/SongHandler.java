8a58820a8496f200c7ddec12e2374d49eeb92643
gCoreByte <21132799+gCoreByte@users.noreply.github.com>
2022-04-06 21:37:02 +0300

    documentation

--- src/main/java/com/miiz/song/SongHandler.java
+++ src/main/java/com/miiz/song/SongHandler.java
@@ -11,19 +11,32 @@ import java.util.Scanner;
 
 import static com.miiz.utils.Utils.tryParse;
 
+/**
+ * Song user interface class
+ * This class handles all user-backend communication for the Song module
+ */
 public class SongHandler {
 
     private final Database database;
     private final Scanner inputReader;
     private final List<Song> songs;
 
+    /**
+     * constructor
+     * @param database - database class to make queries
+     * @param inputReader - a shared input scanner to avoid weird bugs
+     */
     public SongHandler(Database database, Scanner inputReader) {
         this.database = database;
         this.inputReader = inputReader;
+        // initialise the Song list
         this.songs = database.getSongs();
     }
 
-
+    /**
+     * Plays a song if the users platform supports opening a browser
+     * @param song - the song to play
+     */
     public void play(Song song){
         // https://stackoverflow.com/questions/5226212/how-to-open-the-default-webbrowser-using-java
         if (!Desktop.isDesktopSupported() || !Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
@@ -38,6 +51,9 @@ public class SongHandler {
         }
     }
 
+    /**
+     * Lets the user pick a song to play
+     */
     private void pickSong() {
         System.out.println("Valige lugu, mida soovite kuulata:");
         String input = inputReader.nextLine().strip();
@@ -53,6 +69,9 @@ public class SongHandler {
         play(songs.get(choice));
     }
 
+    /**
+     * Lets the user pick a genre from which a song will be played randomly.
+     */
     private void pickSongByGenre() {
         for (int i = 0; i < Genre.genres.size(); i++) {
             System.out.println(i + 1 + ". " + Genre.genres.get(i));
@@ -76,6 +95,9 @@ public class SongHandler {
         play(songsByGenre.get(rand));
     }
 
+    /**
+     * Picks a random song to play.
+     */
     private void pickRandomSong() {
         System.out.println("Esitan juhusliku loo");
         Random r = new Random();
@@ -84,7 +106,11 @@ public class SongHandler {
         play(songs.get(rand));
     }
 
-    public void main(){
+    /**
+     * Main method of SongHandler
+     */
+    public void main() {
+        // no songs found, failed to initialise?
         if (songs.size() == 0) {
             System.out.println("Andmebaasis ei ole ühtegi laulu.");
             System.out.println("Laulude moodul ei tööta.");
@@ -108,8 +134,10 @@ public class SongHandler {
                 case "2" -> pickSong();
                 case "3" -> pickSongByGenre();
                 case "4" -> {
+                    // exit out of module
                     return;
                 }
+                // user input is invalid
                 default -> System.out.println("Pole valiidne sisend");
             }
         }
