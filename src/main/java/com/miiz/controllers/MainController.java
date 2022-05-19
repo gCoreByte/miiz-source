5601444a4aa8617b3d926f8b03a986ff3122006e
gCoreByte <21132799+gCoreByte@users.noreply.github.com>
2022-05-19 04:33:02 +0300

    fixed merge issue

--- src/main/java/com/miiz/controllers/MainController.java
+++ src/main/java/com/miiz/controllers/MainController.java
@@ -1,10 +1,14 @@
 package com.miiz.controllers;
 
-import com.miiz.App;
 import com.miiz.AppNew;
 import com.miiz.auth.UserAuth;
 import com.miiz.notepad.NotepadHandler;
+import com.miiz.song.Genre;
+import com.miiz.song.SongHandler;
+import com.miiz.todolist.ListLine;
+import com.miiz.todolist.ToDoList;
 import com.miiz.todolist.ToDoListHandler;
+import com.miiz.todolist.TodoTree;
 import javafx.beans.binding.ListExpression;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -14,13 +18,12 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.Objects;
@@ -29,15 +32,79 @@ public class MainController {
 
     private ToDoListHandler toDoListHandler;
     private NotepadHandler notepadHandler;
+    private SongHandler songHandler;
+
 
     private final AppNew app;
     public MainController(AppNew app) {
         this.app = app;
-        //this.toDoListHandler = new ToDoListHandler(app.database, app.database.getUser());
+        this.toDoListHandler = new ToDoListHandler(app.database);
         this.notepadHandler = new NotepadHandler(app.database);
+        this.songHandler = new SongHandler(app.database);
 
         app.stage.setResizable(true);
     }
+
+    // todolist
+
+    @FXML
+    private TreeView<TodoTree> todoTree;
+
+    @FXML
+    private TextField todoTitle;
+
+    public void todoChanged() {
+        // TREE TIME
+        TreeItem<TodoTree> rootNode = new TreeItem<>();
+        rootNode.setExpanded(true);
+
+        List<ToDoList> lists = toDoListHandler.getLists();
+        for (ToDoList list : lists) {
+            TreeItem<TodoTree> toDoListTreeItem = new TreeItem<>(list);
+            for (ListLine line : list.getListLines()) {
+                TreeItem<TodoTree> listLineItem = new TreeItem<>(line);
+                toDoListTreeItem.getChildren().add(listLineItem);
+            }
+            rootNode.getChildren().add(toDoListTreeItem);
+        }
+        todoTree.setRoot(rootNode);
+    }
+
+    public void addValue() {
+        if (todoTree.getSelectionModel().getSelectedItem() == null) {
+            toDoListHandler.addList(todoTitle.getText());
+        }
+        else if (todoTree.getSelectionModel().getSelectedItem().getValue() instanceof ToDoList) {
+            toDoListHandler.addLine((ToDoList) todoTree.getSelectionModel().getSelectedItem().getValue(), todoTitle.getText());
+        }
+        todoChanged();
+    }
+
+    public void deleteValue() {
+        if (todoTree.getSelectionModel().getSelectedItem() == null) {
+            return;
+        }
+        if (todoTree.getSelectionModel().getSelectedItem().getValue() instanceof ToDoList) {
+            toDoListHandler.deleteList((ToDoList) todoTree.getSelectionModel().getSelectedItem().getValue());
+        } else if (todoTree.getSelectionModel().getSelectedItem().getValue() instanceof ListLine) {
+            toDoListHandler.deleteLine((ToDoList) todoTree.getSelectionModel().getSelectedItem().getParent().getValue(), (ListLine) todoTree.getSelectionModel().getSelectedItem().getValue());
+        }
+        todoChanged();
+    }
+
+    public void editValue() {
+        if (todoTree.getSelectionModel().getSelectedItem() == null) {
+            return;
+        }
+        if (todoTree.getSelectionModel().getSelectedItem().getValue() instanceof ToDoList) {
+            toDoListHandler.editList((ToDoList) todoTree.getSelectionModel().getSelectedItem().getValue(), todoTitle.getText());
+        } else if (todoTree.getSelectionModel().getSelectedItem().getValue() instanceof ListLine) {
+            toDoListHandler.editLine((ToDoList) todoTree.getSelectionModel().getSelectedItem().getParent().getValue(), (ListLine) todoTree.getSelectionModel().getSelectedItem().getValue(), todoTitle.getText());
+        }
+        todoChanged();
+    }
+
+    // notepad
 
     @FXML
     private TextArea fileContent;
@@ -78,5 +145,40 @@ public class MainController {
     public void deleteFile() {
         notepadHandler.deleteFile(filePicker.getValue());
         notesChanged();
+    }
+
+    // Songs
+
+    @FXML
+    private Button randomSong;
+
+    @FXML
+    private ChoiceBox<String> genrePicker;
+
+    @FXML
+    private ChoiceBox<String> songPicker;
+
+    @FXML
+    private Label latestSong;
+
+    public void pickRandomSong(){
+        String title = songHandler.pickRandomSong();
+        latestSong.setText(title);
+    }
+
+    public void pickByGenre(){
+        ObservableList<String> o = FXCollections.observableArrayList(Genre.genres);
+        genrePicker.setItems(o);
+        String genre = genrePicker.getSelectionModel().getSelectedItem();
+        String title = songHandler.pickSongByGenre(genre);
+        latestSong.setText(title);
+    }
+
+    public void pickSong(){
+        ObservableList<String> o = FXCollections.observableArrayList(songHandler.getSongTitles());
+        songPicker.setItems(o);
+        String songTitle = songPicker.getSelectionModel().getSelectedItem();
+        String title = songHandler.pickSong(songTitle);
+        latestSong.setText(title);
     }
 }
